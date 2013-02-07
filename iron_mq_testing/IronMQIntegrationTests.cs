9e6c093cd08093a9c4e6a18b8a81e83dd01ccfd9
eskat0n <muyou.prj@gmail.com>
2013-02-07 10:43:19 +0600

    cleanup for tests

--- iron_mq_testing/IronMQIntegrationTests.cs
+++ iron_mq_testing/IronMQIntegrationTests.cs
@@ -1,55 +1,134 @@
 ﻿namespace IronMQ
 {
+    using System;
     using System.Configuration;
+    using System.Globalization;
     using System.Linq;
-    using Data;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NUnit.Framework;
 
-    [TestClass]
+    [TestFixture]
     public class IronMQIntegrationTests
     {
-        private string _projectId = null;
-        private string _token = null;
+        private const string TestQueueName = "test-queue";
+
+        private readonly string _projectId;
+        private readonly string _token;
 
         public IronMQIntegrationTests()
         {
             _projectId = ConfigurationManager.AppSettings["IRONIO_PROJECT_ID"];
             _token = ConfigurationManager.AppSettings["IRONIO_TOKEN"];
+
             Assert.IsFalse(string.IsNullOrWhiteSpace(_projectId));
             Assert.IsFalse(string.IsNullOrWhiteSpace(_token));
         }
 
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
+        [Test]
+        public void BasicTests()
         {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
+            var client = new Client(_projectId, _token);
+            var queue = client.queue(TestQueueName);
+
+            ClearQueue(queue);
+            // clear_queue
+            queue.push("hello world!");
+            var message = queue.get();
+            Assert.IsNotNull(message.Id);
+            Assert.IsNotNull(message.Body);
+
+            queue.deleteMessage(message.Id);
+            message = queue.get();
+            Assert.IsNull(message);
+
+            queue.push("hello world 2!");
+
+            message = queue.get();
+            Assert.IsNotNull(message);
+
+            queue.deleteMessage(message.Id);
+
+            message = queue.get();
+            Assert.IsNull(message);
         }
 
-        [TestMethod]
+        [Test]
+        public void BulkGetTest()
+        {
+            var client = new Client(_projectId, _token);
+            var queue = client.queue(TestQueueName);
+            ClearQueue(queue);
+
+            var messages = Enumerable.Range(0, 10)
+                .Select(i => i.ToString(CultureInfo.InvariantCulture))
+                .ToArray();
+            queue.push(messages);
+
+            var actual = queue.get(100);
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.Count > 1);
+        }
+
+        [Test]
+        public void BulkPushTest()
+        {
+            var client = new Client(_projectId, _token);
+            var queue = client.queue(TestQueueName);
+
+            ClearQueue(queue);
+            var messages = Enumerable.Range(0, 10)
+                .Select(i => i.ToString(CultureInfo.InvariantCulture))
+                .ToArray();
+            queue.push(messages);
+
+            for (var i = 0; i < 10; i++)
+            {
+                var message = queue.get();
+                Assert.IsNotNull(message);
+            }
+            // Assumption is that if we queued up 1000 and we got back 1000 then it worked fine.
+            // Note: this does not verify we got back the same messages
+        }
+
+        [Test]
+        public void ClearEmptyQueueTest()
+        {
+            var client = new Client(_projectId, _token);
+            var queue = client.queue(TestQueueName);
+            ClearQueue(queue);
+            // At this point the queue should be empty
+            queue.clear();
+        }
+
+        [Test]
+        public void ClearQueueTest()
+        {
+            const string messageBody = "This is a test of the emergency broadcasting system... Please stand by...";
+
+            var client = new Client(_projectId, _token);
+            var queue = client.queue(TestQueueName);
+            ClearQueue(queue);
+
+            queue.push(messageBody);
+            queue.clear();
+
+            var message = queue.get();
+            Assert.IsNull(message);
+        }
+
+        [Test]
         public void TestMethod1()
         {
-            Client c = new Client(_projectId, _token);
-            Queue q = c.queue("test-queue");
-            ClearQueue(q);
+            const string body = "Hello, IronMQ!";
 
-            string body = "Hello, IronMQ!";
-            q.push(body);
+            var client = new Client(_projectId, _token);
+            var queue = client.queue(TestQueueName);
+            ClearQueue(queue);
 
-            Message msg = q.get();
-            Assert.IsTrue(string.Compare(body, msg.Body) == 0);
-            q.deleteMessage(msg);
+            queue.push(body);
+
+            var message = queue.get();
+            Assert.IsTrue(String.CompareOrdinal(body, message.Body) == 0);
+            queue.deleteMessage(message);
         }
 
         private void ClearQueue(Queue q)
@@ -58,109 +137,10 @@
             {
                 q.clear();
             }
-            catch { 
+            catch
+            {
                 //TODO: This is here because of a bug in the Endpoint where clearing an empty queue results in a 500 internal server error.
             }
-        }
-
-        [TestMethod]
-        public void BasicTests()
-        {
-            Client c = new Client(_projectId, _token);
-            Queue q = c.queue("test-queue");
-
-            ClearQueue(q);
-            // clear_queue
-            q.push("hello world!");
-            Message msg = q.get();
-            Assert.IsNotNull(msg.Id);
-            Assert.IsNotNull(msg.Body);
-
-            q.deleteMessage(msg.Id);
-            msg = q.get();
-            Assert.IsNull(msg);
-            
-            q.push("hello world 2!");
-
-            msg = q.get();
-            Assert.IsNotNull(msg);
-
-
-            q.deleteMessage(msg.Id);
-
-
-            msg = q.get();
-            Assert.IsNull(msg);
-
-        }
-
-        /// <summary>
-        ///A test for push
-        ///</summary>
-        [TestMethod()]
-        public void BulkPushTest()
-        {
-            Client c = new Client(_projectId, _token);
-            Queue q = c.queue("test-queue");
-
-            ClearQueue(q);
-            var messages = Enumerable.Range(0, 10).Select(i => i.ToString()).ToArray();
-            long timeout = 0; 
-            q.push(messages, timeout);
-
-            for (int i = 0; i < 10; i++)
-            {
-                var msg = q.get();
-                Assert.IsNotNull(msg);
-            }
-            // Assumption is that if we queued up 1000 and we got back 1000 then it worked fine.
-            // Note: this does not verify we got back the same messages
-         
-        }
-        [TestMethod]
-        public void BulkGetTest()
-        {
-            Client c = new Client(_projectId, _token);
-            Queue q = c.queue("test-queue");
-            ClearQueue(q);
-
-            var messages = Enumerable.Range(0, 10).Select(i => i.ToString()).ToArray();
-            long timeout = 0;
-            q.push(messages, timeout);
-
-            var actual = q.get(100);
-            Assert.IsNotNull(actual);
-            Assert.IsTrue(actual.Count > 1);
-        }
-        /// <summary>
-        /// Test for clearing a queue.
-        /// </summary>
-        [TestMethod]
-        public void ClearQueueTest()
-        {
-            Client c = new Client(_projectId, _token);
-            Queue q = c.queue("test-queue");
-            ClearQueue(q);
-
-            var messageBody = "This is a test of the emergency broadcasting system... Please stand by...";
-            q.push(messageBody);
-            q.clear();
-
-            var msg = q.get();
-            Assert.IsNull(msg);
-        }
-
-        /// <summary>
-        /// Test For Clearing an empty queue
-        /// </summary>
-        [TestMethod]
-        public void ClearEmptyQueueTest()
-        {
-            Client c = new Client(_projectId, _token);
-            Queue q = c.queue("test-queue");
-            ClearQueue(q); 
-            // At this point the queue should be empty
-            q.clear();
         }
     }
 }
